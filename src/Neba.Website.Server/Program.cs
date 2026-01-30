0ac66ae3699526b50789f6d1388fbdf2c3204063
David Kipperman <kippermand@gmail.com>
2026-01-30 15:14:01 -0500

    Extract API service configuration to extension
    Move inline API setup out of Program.cs into a new ApiServicesConfiguration extension. Adds AddApiServices(configuration) which binds and validates NebaApiConfiguration, registers ApiExecutor, centralizes Refit settings (System.Text.Json with camelCase), and provides a generic RegisterApiEndpoint helper to register IWeatherApi -> WeatherService. Program.cs now calls AddApiServices(builder.Configuration) instead of configuring the API clients inline.

--- src/Neba.Website.Server/Program.cs
+++ src/Neba.Website.Server/Program.cs
@@ -1,51 +1,14 @@
-using System.Text.Json;
-
-using Microsoft.Extensions.Options;
-
-using Neba.Api.Contracts.Weather;
 using Neba.Website.Server.BackgroundJobs;
 using Neba.Website.Server.Clock;
 using Neba.Website.Server.Components;
 using Neba.Website.Server.Services;
-using Neba.Website.Server.Weather;
-
-using Refit;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
 
-builder.Services.AddBackgroundJobs(builder.Configuration);
-
-// look to move this to api configuration
-
-builder.Services.AddOptions<NebaApiConfiguration>()
-    .Bind(builder.Configuration.GetSection("NebaApi"))
-    .ValidateDataAnnotations()
-    .ValidateOnStart();
-
-builder.Services.AddSingleton(sp => sp.GetRequiredService<IOptions<NebaApiConfiguration>>().Value);
-
-var jsonSerializerOptions = new JsonSerializerOptions
-{
-    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-};
-
-builder.Services
-    .AddRefitClient<IWeatherApi>(new RefitSettings
-    {
-        ContentSerializer = new SystemTextJsonContentSerializer(jsonSerializerOptions),
-    })
-    .ConfigureHttpClient((sp, client) =>
-    {
-        var apiConfig = sp.GetRequiredService<NebaApiConfiguration>();
-        client.BaseAddress = new Uri(apiConfig.BaseUrl);
-    });
-
-builder.Services.AddScoped<WeatherService>(); // the add refit client can be refactored to extension and tied w/ the service
-
-// end api configuration refactoring opportunity
+builder.Services.AddApiServices(builder.Configuration);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
---
+++ src/Neba.Website.Server/Services/ApiServicesConfiguration.cs
@@ -0,0 +1,61 @@
+using System.Text.Json;
+
+using Microsoft.Extensions.Options;
+
+using Neba.Api.Contracts.Weather;
+using Neba.Website.Server.Weather;
+
+using Refit;
+
+namespace Neba.Website.Server.Services;
+
+#pragma warning disable S1144 // Interfaces should not have public constructors
+#pragma warning disable S2325 // Private types or members should not be static
+
+internal static class ApiServicesConfiguration
+{
+    private static readonly RefitSettings RefitSettings = new()
+    {
+        ContentSerializer = new SystemTextJsonContentSerializer(
+            new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            }
+        )
+    };
+
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddApiServices(IConfiguration configuration)
+        {
+            services.AddOptions<NebaApiConfiguration>()
+                .Bind(configuration.GetSection("NebaApi"))
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+            services.AddSingleton(sp => sp.GetRequiredService<IOptions<NebaApiConfiguration>>().Value);
+            services.AddScoped<ApiExecutor>();
+
+            services.RegisterApiEndpoint<IWeatherApi, WeatherService>();
+
+            return services;
+        }
+
+        private IServiceCollection RegisterApiEndpoint<TApi, TService>()
+            where TApi : class
+            where TService : class
+        {
+            services
+                .AddRefitClient<TApi>(RefitSettings)
+                .ConfigureHttpClient((sp, client) =>
+                {
+                    var apiConfig = sp.GetRequiredService<NebaApiConfiguration>();
+                    client.BaseAddress = new Uri(apiConfig.BaseUrl);
+                });
+
+            services.AddScoped<TService>();
+
+            return services;
+        }
+    }
+}
