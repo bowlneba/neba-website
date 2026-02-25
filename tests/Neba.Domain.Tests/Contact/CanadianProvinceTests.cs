bd32020c03d0cbac5202a2eaf3d0282a0ef52db3
David Kipperman <kippermand@gmail.com>
2026-02-25 07:23:25 -0500

    Add CanadianProvince SmartEnum and tests
    Introduce CanadianProvince as an Ardalis.SmartEnum representing the 13 Canadian provinces and territories with their two-letter abbreviations (AB, BC, MB, NB, NL, NS, ON, PE, QC, SK, NT, NU, YT). Add unit tests (xUnit + Shouldly) verifying the enum contains 13 entries and that FromValue returns the correct Name and Value for each province/territory.

---
+++ tests/Neba.Domain.Tests/Contact/CanadianProvinceTests.cs
@@ -0,0 +1,43 @@
+using Neba.Domain.Contact;
+
+using Shouldly;
+
+using Xunit;
+
+namespace Neba.Domain.Tests.Contact;
+
+global using Xunit;
+
+global using Shouldly;
+
+[UnitTest]
+[Component("Contact.CanadianProvince")]
+public sealed class CanadianProvinceTests
+{
+    [Fact(DisplayName = "Should have 13 Canadian provinces and territories")]
+    public void CanadianProvince_ShouldHave13Provinces()
+    {
+        CanadianProvince.List.Count.ShouldBe(13);
+    }
+
+    [Theory(DisplayName = "Province and territory abbreviations should be correct")]
+    [InlineData("Alberta", "AB", TestDisplayName = "Alberta abbreviation should be AB")]
+    [InlineData("British Columbia", "BC", TestDisplayName = "British Columbia abbreviation should be BC")]
+    [InlineData("Manitoba", "MB", TestDisplayName = "Manitoba abbreviation should be MB")]
+    [InlineData("New Brunswick", "NB", TestDisplayName = "New Brunswick abbreviation should be NB")]
+    [InlineData("Newfoundland and Labrador", "NL", TestDisplayName = "Newfoundland and Labrador abbreviation should be NL")]
+    [InlineData("Nova Scotia", "NS", TestDisplayName = "Nova Scotia abbreviation should be NS")]
+    [InlineData("Ontario", "ON", TestDisplayName = "Ontario abbreviation should be ON")]
+    [InlineData("Prince Edward Island", "PE", TestDisplayName = "Prince Edward Island abbreviation should be PE")]
+    [InlineData("Quebec", "QC", TestDisplayName = "Quebec abbreviation should be QC")]
+    [InlineData("Saskatchewan", "SK", TestDisplayName = "Saskatchewan abbreviation should be SK")]
+    [InlineData("Northwest Territories", "NT", TestDisplayName = "Northwest Territories abbreviation should be NT")]
+    [InlineData("Nunavut", "NU", TestDisplayName = "Nunavut abbreviation should be NU")]
+    [InlineData("Yukon", "YT", TestDisplayName = "Yukon abbreviation should be YT")]
+    public void CanadianProvince_ShouldHaveCorrectProperties(string provinceName, string expectedAbbreviation)
+    {
+        var province = CanadianProvince.FromValue(expectedAbbreviation);
+        province.Name.ShouldBe(provinceName);
+        province.Value.ShouldBe(expectedAbbreviation);
+    }
+}
