df96f0464430d5aecbe5c5b9cac6fcab773fcf87
David Kipperman <kippermand@gmail.com>
2026-02-28 13:22:02 -0500

    Add PinFallType smart enum and tests
    Introduce PinFallType as a SmartEnum to represent pinsetter mechanism types for tenpin lanes (Free Fall "FF" and String Pin "SP"). Adds unit tests to verify there are exactly two types and that FromValue maps values to the correct Name/Value properties. Uses Ardalis.SmartEnum and lives under Neba.Domain.BowlingCenters.

---
+++ tests/Neba.Domain.Tests/BowlingCenters/PinFallTypeTests.cs
@@ -0,0 +1,25 @@
+using Neba.Domain.BowlingCenters;
+using Neba.TestFactory.Attributes;
+
+namespace Neba.Domain.Tests.BowlingCenters;
+
+[UnitTest]
+[Component("BowlingCenters.PinFallType")]
+public sealed class PinFallTypeTests
+{
+    [Fact(DisplayName = "Should have 2 pin fall types")]
+    public void PinFallType_ShouldHave2Types()
+    {
+        PinFallType.List.Count.ShouldBe(2);
+    }
+
+    [Theory(DisplayName = "Pin fall type values should be correct")]
+    [InlineData("Free Fall", "FF", TestDisplayName = "Free Fall value should be FF")]
+    [InlineData("String Pin", "SP", TestDisplayName = "String Pin value should be SP")]
+    public void PinFallType_ShouldHaveCorrectProperties(string expectedName, string value)
+    {
+        var type = PinFallType.FromValue(value);
+        type.Name.ShouldBe(expectedName);
+        type.Value.ShouldBe(value);
+    }
+}
