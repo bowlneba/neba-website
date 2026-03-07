77982bc0b55f72aa4ac05f546457c4be7344cd38
David Kipperman <kippermand@gmail.com>
2026-03-07 13:24:06 -0500

    Fix telemetry helper import path
    Update NebaMap.razor.js to import telemetry-helper from ../js instead of ../wwwroot/js to match the module's new location. This prevents module resolution/runtime errors when using trackError and createTimer.

--- src/Neba.Website.Server/Maps/NebaMap.razor.js
+++ src/Neba.Website.Server/Maps/NebaMap.razor.js
@@ -2,7 +2,7 @@
 // Displays locations on an interactive map with clustering, popups, and focus capabilities
 // Note: Assumes 'atlas' is available globally from the Azure Maps SDK CDN
 
-import { trackError, createTimer } from "../wwwroot/js/telemetry-helper";
+import { trackError, createTimer } from "../js/telemetry-helper";
 
 let map = null;
 let dataSource = null;
