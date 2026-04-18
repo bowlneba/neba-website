90c946ec468d76403c1e2707fc2e17498b861f53
David Kipperman <kippermand@gmail.com>
2026-04-18 18:14:04 -0400

    Add Uncertified bowling center status
    Introduce a new BowlingCenterStatus: Uncertified (value 2) with XML documentation describing its meaning and use for filtering or informational purposes. Update tests to expect three statuses and add an assertion (InlineData) verifying the Uncertified name/value.

--- tests/Neba.Domain.Tests/BowlingCenters/BowlingCenterStatusTests.cs
+++ tests/Neba.Domain.Tests/BowlingCenters/BowlingCenterStatusTests.cs
@@ -7,19 +7,20 @@ namespace Neba.Domain.Tests.BowlingCenters;
 [Component("BowlingCenters.BowlingCenterStatus")]
 public sealed class BowlingCenterStatusTests
 {
-    [Fact(DisplayName = "Should have 2 bowling center statuses")]
-    public void BowlingCenterStatus_ShouldHave2Statuses()
+    [Fact(DisplayName = "Should have 3 bowling center statuses")]
+    public void BowlingCenterStatus_ShouldHave3Statuses()
     {
         // Act
         var count = BowlingCenterStatus.List.Count;
 
         // Assert
-        count.ShouldBe(2);
+        count.ShouldBe(3);
     }
 
     [Theory(DisplayName = "Bowling center status values should be correct")]
     [InlineData("Open", 0, TestDisplayName = "Open value should be 0")]
     [InlineData("Closed", 1, TestDisplayName = "Closed value should be 1")]
+    [InlineData("Uncertified", 2, TestDisplayName = "Uncertified value should be 2")]
     public void BowlingCenterStatus_ShouldHaveCorrectProperties(string expectedName, int expectedValue)
     {
         // Act
