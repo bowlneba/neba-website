415876f68a43c4b35df9e77d8dba09a58c7516be
David Kipperman <kippermand@gmail.com>
2026-05-06 11:22:26 -0400

    Add SideCut migration and update snapshot
    Add new EF Core migration SideCut_Init that creates side_cuts, side_cut_criteria_groups and side_cut_criteria tables (with PKs, alternate keys, FKs and indexes) under the "app" schema and corresponding Up/Down logic. Include the generated SideCut_Init.Designer.cs BuildTargetModel snapshot. Update AppDbContextModelSnapshot ProductVersion to 10.0.7 and append SideCut entities to the model snapshot.

--- src/Neba.Infrastructure/Database/Migrations/20260506151352_SideCut_Init.Designer.cs
+++ src/Neba.Infrastructure/Database/Migrations/20260506151352_SideCut_Init.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Neba.Infrastructure.Database;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Neba.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260506151352_SideCut_Init")]
+    partial class SideCut_Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
