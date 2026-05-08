0990d06f3776f0d55043b409af4631486ae1c2e7
David Kipperman <kippermand@gmail.com>
2026-05-08 13:01:57 -0400

    Husky: dotnet format

--- src/Neba.Application/Tournaments/TournamentErrors.cs
+++ src/Neba.Application/Tournaments/TournamentErrors.cs
@@ -8,7 +8,7 @@ internal static class TournamentErrors
 {
     public static Error TournamentNotFound(TournamentId id)
         => Error.NotFound(
-            code: "Tournament.NotFound", 
+            code: "Tournament.NotFound",
             description: "Tournament was not found.",
             metadata: new Dictionary<string, object>
             {
