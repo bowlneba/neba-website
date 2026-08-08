c6a7ab3af71012f1f821c4447c0dfdc0bcc6b2ef
David Kipperman <kippermand@gmail.com>
2026-08-08 09:38:22 -0400

    Update LegacyEndpoints.cs

--- src/Neba.Api/Legacy/LegacyEndpoints.cs
+++ src/Neba.Api/Legacy/LegacyEndpoints.cs
@@ -6,11 +6,9 @@ internal static class LegacyEndpoints
 {
     extension(IEndpointRouteBuilder app)
     {
-#pragma warning disable CA1822
         public void MapLegacyEndpoints()
         {
             app.MapNewBowler();
         }
-#pragma warning restore CA1822
     }
 }
