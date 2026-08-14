26a362bce6aa382f97a16484d5202c3026059951
David Kipperman <kippermand@gmail.com>
2026-08-14 13:22:23 -0400

    squad configuration

---
+++ src/Neba.Api/Database/Configurations/SquadConfiguration.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using Neba.Api.Features.Tournaments.Domain;
+
+namespace Neba.Api.Database.Configurations;
+
+internal sealed class SquadConfiguration
+    : IEntityTypeConfiguration<Squad>
+{
+    public void Configure(EntityTypeBuilder<Squad> builder)
+    {
+        builder.ToTable("squads", AppDbContext.DefaultSchema);
+
+        builder.ConfigureShadowId();
+
+        builder.Property(squad => squad.Id)
+            .IsUlid();
+
+        builder.HasAlternateKey(squad => squad.Id);
+
+        builder.Property<int>(TournamentConfiguration.ForeignKeyName)
+            .IsRequired();
+
+        builder.HasOne<Tournament>()
+            .WithMany(tournament => tournament.Squads)
+            .HasForeignKey(TournamentConfiguration.ForeignKeyName)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(squad => squad.BowlingDateTime)
+            .HasColumnName("bowling_date_time")
+            .IsRequired();
+
+        builder.Property(squad => squad.MaxEntries)
+            .HasColumnName("max_entries");
+
+        builder.Property(squad => squad.LegacyId)
+            .ValueGeneratedNever();
+
+        builder.HasIndex(squad => squad.LegacyId)
+            .IsUnique()
+            .AreNullsDistinct();
+    }
+}
